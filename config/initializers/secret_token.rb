5b2c1140095e93fdbb43f45cb60a86ed3cb6c92d
Gilliard <gilalan@gmail.com>
2012-12-06 20:05:39 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuadritApp::Application.config.secret_token = '60f8c82476251175a532b8faeb976f91fb6e6b3feab01f025579027e91b5b9904779348c3fc97fe05b1cec8cfd3c3de16cd427e9da50cf1435c74de585a605c0'
